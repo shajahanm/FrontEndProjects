e7ed2641340fcaf1e3d4a1f3f77255b8678b2ed9
shajahanm <95293570+shajahanm@users.noreply.github.com>
2025-04-03 21:23:19 +0530

    array oprations

---
+++ data-structure/array-operation.js
@@ -0,0 +1,8 @@
+const findLargest = (arr) => {
+  return Math.max(arr);
+};
+const findMinimum = (arr) => {
+  return Math.min(arr);
+};
+
+const sampleArray = [64, 10, 34, 50, 90, 12, 3, 39, 1];
--- data-structure/min-max.js
+++ data-structure/min-max.js
@@ -2,4 +2,4 @@ function findMinMax(arr) {
   return { min: Math.min(...arr), max: Math.max(...arr) };
 }
 
-console.log(findMinMax([10, 20, 5, 40])); // Output: { min: 5, max: 40 }
+console.log(findMinMax([30, 20, 5, 40])); // Output: { min: 5, max: 40 }
