e2182ec7a675c5e47157ac5ca9a271c5c364805c
shajahanm <95293570+shajahanm@users.noreply.github.com>
2025-04-08 22:21:34 +0530

    get total

--- data-structure/prototype.js
+++ data-structure/prototype.js
@@ -9,3 +9,17 @@ Person.prototype.greet = function () {
 
 const john = new Person("John", 25);
 john.greet();
+
+Array.prototype.addNumbers = function () {
+  let value = 0;
+  const allNumbers = this.every((item) => typeof item === "number");
+  if (!allNumbers) {
+    throw new Error("Array contains non-number elements");
+  }
+  for (let i = 0; i < this.length; i++) {
+    value = value + this[i];
+  }
+  return value
+};
+
+console.log([1, 4, 30, 20, 5].addNumbers());
