74c1322dd9a9b8107501adcfcb1ec4fcf15c8b94
Shajahan M <shajahanm8866@gmail.com>
2025-04-28 14:33:08 +0530

    project initial set up

---
+++ react-firebase-chat-starter/src/components/list/userInfo/UserInformation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './user-information.css'
+const UserInformation = () => {
+  return <div className="user-information">UserInformation</div>;
+};
+
+export default UserInformation;
