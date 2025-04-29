a315ee125eca3f2cb268d934841dd196c938069b
Shajahan M <shajahanm8866@gmail.com>
2025-04-29 14:44:40 +0530

    firbase config

---
+++ react-firebase-chat-starter/src/lib/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import {getFireStore} from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API,
+  authDomain: "reactchat-e2513.firebaseapp.com",
+  projectId: "reactchat-e2513",
+  storageBucket: "reactchat-e2513.firebasestorage.app",
+  messagingSenderId: "96938237046",
+  appId: "1:96938237046:web:ccb74d0a32fc8bc3475357"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFireStore()
